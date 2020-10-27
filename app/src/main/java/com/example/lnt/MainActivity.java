6a542b485872186c136b12569f609c629e421ef3
pratheeksha <pratheeksha.j@ltts.com>
2020-10-27 15:46:18 +0530

    Start another activity and send data

--- app/src/main/java/com/example/lnt/MainActivity.java
+++ app/src/main/java/com/example/lnt/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.lnt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.time.Instant;
 
 public class MainActivity extends AppCompatActivity {
     EditText nameEditText; //declaration
@@ -18,7 +21,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clickHandler(View view) {
-        String name = nameEditText.getText().toString(); //using
-        Toast.makeText(this, "Welcome to Android "+name, Toast.LENGTH_SHORT).show();
+        switch (view.getId()){
+            case R.id.buttonlogin:
+                String name = nameEditText.getText().toString(); //using
+                //Toast.makeText(this, "Welcome to Android "+name, Toast.LENGTH_SHORT).show();
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                homeIntent.putExtra("mykey",name);
+                startActivity(homeIntent);
+                break;
+            case R.id.buttoncancel:
+                Toast.makeText(this, "cancelled ", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+
     }
 }
