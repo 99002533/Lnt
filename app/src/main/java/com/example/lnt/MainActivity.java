90b07c4b490b89cf93755b2ffcba3f6bb128408e
pratheeksha <pratheeksha.j@ltts.com>
2020-10-28 10:27:32 +0530

    activity lifecycle

--- app/src/main/java/com/example/lnt/MainActivity.java
+++ app/src/main/java/com/example/lnt/MainActivity.java
@@ -23,6 +23,40 @@ public class MainActivity extends AppCompatActivity {
         nameEditText = findViewById(R.id.editTextTextPersonName); //init
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG,"activity started");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG,"activity paused");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG,"activity resumed");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG,"activity stopped");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"activity destroyed");
+    }
+
+
+
+
+
     public void clickHandler(View view) {
         Log.v(TAG, "button clicked");
         switch (view.getId()){
@@ -31,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
                 String name = nameEditText.getText().toString(); //using
                 //Toast.makeText(this, "Welcome to Android "+name, Toast.LENGTH_SHORT).show();
                 Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
-                homeIntent.putExtra("mykey",name);
+               homeIntent.putExtra("mykey",name);
                 startActivity(homeIntent);
                 break;
             case R.id.buttoncancel:
