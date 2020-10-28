86a7b369f2f5bc7dfa8c06eeefeba0f295fbc712
pratheeksha <pratheeksha.j@ltts.com>
2020-10-28 17:32:05 +0530

    options menu

--- app/src/main/java/com/example/lnt/RecyclerActivity.java
+++ app/src/main/java/com/example/lnt/RecyclerActivity.java
@@ -1,11 +1,16 @@
 package com.example.lnt;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.lnt.override.NotesAdapter;
 
@@ -41,5 +46,27 @@ public class RecyclerActivity extends AppCompatActivity {
         noteArrayList.add(new Note("title7","subtitle7"));
         noteArrayList.add(new Note("title8","subtitle8"));
 
+    }
+    @Override // to show the menu
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.recycler_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.selectall:
+                Toast.makeText(this, "selecting all", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.paste:
+                Toast.makeText(this, "pasting all", Toast.LENGTH_SHORT).show();
+
+                break;
+        }
+        return true;
     }
 }
